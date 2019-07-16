ba3654619d3f6ef51c489ccf513d7b6b4a378d69
Kelly Zick <kllyzck@gmail.com>
2019-07-16 16:35:21 -0600

    Connect scss partials, fix style sheet import

---
+++ src/components/App/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <main>
+      <h1>InCrisis</h1>
+      {/* <Header /> */}
+      {/* <HomePage /> */}
+      {/* <MapPage /> */}
+      {/* <MoreInfoPage /> */}
+    </main>
+  )
+}
+
+export default App;
