f8d1b9bfd36653b8b7f2965e702be9f45e28a745
Brennan <brennanduffey@gmail.com>
2019-07-17 16:37:57 -0600

    Refactor atlas component move state to mappage

--- src/components/Atlas/index.js
+++ src/components/Atlas/index.js
@@ -1,29 +1,16 @@
-import React, { useEffect, useState} from 'react';
+import React from 'react';
 import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
 
 
 
-function Atlas () {
-  const lat = 39.780760;
-  const long = -105.082270;
-  const originalCenter = [lat, long];
-
-  const [userLocation, setUserLocation] = useState(originalCenter);
-  
-  useEffect(() => {
-    window.navigator.geolocation.getCurrentPosition((position) => 
-      setUserLocation([position.coords.latitude, position.coords.longitude])
-    );
-  });
-  
-  
+const Atlas = (props) => {
   return (
-    <Map id="mapid" center={userLocation} zoom={13}>
+    <Map id="mapid" center={props.userLocation} zoom={13}>
       <TileLayer
         attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={userLocation}>
+      <Marker position={props.userLocation}>
         <Popup>Info about center</Popup>
       </Marker>
     </Map>
