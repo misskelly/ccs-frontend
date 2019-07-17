e0817e5657d78c1111091c2c338898428f8b2d95
Brennan <brennanduffey@gmail.com>
2019-07-17 11:37:02 -0600

    Correct atlas name and install leaflet

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Atlas from '../Atlas'
 
 const App = () => {
   return (
     <main>
       <h1>InCrisis</h1>
+      <Atlas />
       {/* <Header /> */}
       {/* <HomePage /> */}
       {/* <MapPage /> */}
