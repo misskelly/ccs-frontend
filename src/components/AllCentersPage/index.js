38952ed86080c9abb3b0e4ce4799f4bb0b824a7c
Kelly Zick <kllyzck@gmail.com>
2019-07-17 09:44:51 -0600

    Add snapshot tests and linter config

---
+++ src/components/AllCentersPage/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const AllCentersPage = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default AllCentersPage;
---
+++ src/components/App/index.test.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from '.';
+
+
+describe('App', () => {
+  let wrapper;
+
+  it('should match the snapshot', () => {
+    wrapper = shallow(<App />);
+
+    expect(wrapper).toMatchSnapshot();
+  });
+});
