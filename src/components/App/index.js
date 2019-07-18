f16dcf10b3adc43ea3a23ace2ce52c49b11509f0
Kelly Zick <kllyzck@gmail.com>
2019-07-17 20:04:10 -0600

    Style Header, add Menu component

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -4,7 +4,7 @@ import Header from '../Header';
 import HomePage from '../HomePage';
 import MapPage from '../MapPage';
 import AllCentersPage from '../AllCentersPage';
-import MoreInfoPage from '../MoreInfoPage';
+// import MoreInfoPage from '../MoreInfoPage';
 
 const App = () => {
   return (
@@ -13,8 +13,8 @@ const App = () => {
       <Switch>
         <Route exact path='/' component={ HomePage } />
         <Route path='/walk-in' component={ MapPage } />
-        <Route path='/more-info' component={ MoreInfoPage } />
         <Route path='/all-centers' component={ AllCentersPage } />
+        {/* <Route path='/more-info' component={ MoreInfoPage } /> */}
         {/* <Route path='/' component={ ErrorPage } /> */}
       </Switch>
     </>
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import CCSlogo from  '../../assets/images/CCS_Horizontal_white.png';
+import Menu from '../Menu'
 
 const Header = () => {
   return (
     <header className="header">
-      HEADER
+      <NavLink
+        className="ccs-logo-wrapper"
+        to="/">
+        <img 
+          src={CCSlogo}
+          className="ccs-logo-img"
+          alt="Colorado Crisis Services logo with swirly tree, link to home page"
+        />
+      </NavLink>
+      <Menu />
     </header>
   )
 }
