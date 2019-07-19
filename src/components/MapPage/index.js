9fc62848de43773a2dcc74a033192cf62ee80968
Brennan <brennanduffey@gmail.com>
2019-07-19 08:22:27 -0600

    Add links to google maps directions

--- src/components/MapPage/index.js
+++ src/components/MapPage/index.js
@@ -6,7 +6,18 @@ const MapPage = () => {
   const [canEnterAddress, setCanEnterAddress] = useState(false);
   const [userAddress, setUserAddress] = useState('');
   const [userLocation, setUserLocation] = useState([]);
-  const [locations, setLocations] = useState([]);
+  const [closestLocation, setClosestLocation] = useState({
+      "id": 1,
+      "name": "Denver Walk-In Crisis Services",
+      "phone": 3035041299,
+      "address": "4353 E. Colfax Avenue",
+      "city": "Denver", 
+      "state": "CO", 
+      "zip": 80220,
+      "hours": "24 hours a day, 7 days a week",
+      "lat": 39.7403,
+      "lng": -104.9363
+    });
 
   const getAndFetchUserLocation = () => {
     if (window.navigator.geolocation) {
@@ -23,6 +34,12 @@ const MapPage = () => {
   const fetchLocations = () => {
     console.log('Fetchy fetch fetch')
   };
+
+  const drivingUrl = `https://www.google.com/maps/dir/?api=1&origin=${userLocation[0]},${userLocation[1]}&destination=${closestLocation.lat},${closestLocation.lng}&travelmode=driving`;
+  const walkingUrl = `https://www.google.com/maps/dir/?api=1&origin=${userLocation[0]},${userLocation[1]}&destination=${closestLocation.lat},${closestLocation.lng}&travelmode=walking`;
+  const transitUrl = `https://www.google.com/maps/dir/?api=1&origin=${userLocation[0]},${userLocation[1]}&destination=${closestLocation.lat},${closestLocation.lng}&travelmode=transit`;
+  const bikingUrl = `https://www.google.com/maps/dir/?api=1&origin=${userLocation[0]},${userLocation[1]}&destination=${closestLocation.lat},${closestLocation.lng}&travelmode=bicycling`;
+
 
   return (
     <section>
@@ -41,6 +58,22 @@ const MapPage = () => {
         </form>
       }
       {userLocation.length > 0 && <Atlas userLocation={userLocation} />}
+      <p>Center is {closestLocation.distance} miles away</p>
+      <section> 
+        <h3>GET DIRECTIONS</h3>
+        <a href={drivingUrl} target="_blank" rel="noopener noreferrer">
+          <img src={require("../../assets/images/icons/car.svg")} alt="car directions link"/>
+        </a>
+        <a href={walkingUrl} target="_blank" rel="noopener noreferrer">
+          <img src={require("../../assets/images/icons/walk.svg")} alt="walking directions link"/>
+        </a>
+        <a href={bikingUrl} target="_blank" rel="noopener noreferrer">
+          <img src={require("../../assets/images/icons/bike.svg")} alt="biking directions link"/>
+        </a>
+        <a href={transitUrl} target="_blank" rel="noopener noreferrer">
+          <img src={require("../../assets/images/icons/bus.svg")} alt="public transit directions link"/>
+        </a>
+      </section>
     </section>
   );
 };
