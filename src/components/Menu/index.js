a072346b2dc5f1e7a17f01e61a746792a13c5afc
Kelly Zick <kllyzck@gmail.com>
2019-07-23 00:09:42 -0600

    Style dropdown menu

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,38 +1,39 @@
 import React from 'react';
-import { Wrapper, Button, Menu, MenuItem } from 'react-aria-menubutton';
+import {
+  Wrapper, Button, Menu, MenuItem
+} from 'react-aria-menubutton';
 import { NavLink } from 'react-router-dom';
-import MenuIcon from '../../assets/images/icons/menu.svg'
-import { menuItemText } from '../../utils/text.js'
+import MenuIcon from '../../assets/images/icons/menu.svg';
+import { menuItemText } from '../../utils/text.js';
 
 
 const MenuButton = () => {
-  const menuItems = menuItemText.map((item, i) => {
-    return (
-      <li key={i}>
-        <MenuItem className={ item.class }>
-          <NavLink
-            className={`nav-${ item.class }`}
-            to={ item.path }>
-              { item.text }
-          </NavLink>
-        </MenuItem>
-      </li>
-    );
-  });
+  const menuItems = menuItemText.map((item, i) => (
+    <li className="menu-li" key={i}>
+      <MenuItem className={item.class}>
+        <NavLink
+          className={`nav-menu nav-${item.class}`}
+          to={item.path}
+        >
+          { item.text }
+        </NavLink>
+      </MenuItem>
+    </li>
+  ));
 
   return (
     <Wrapper
-      className='menu-btn-wrapper'
+      className="menu-btn-wrapper"
     >
-      <Button className='menu-btn'>
-        <img 
-        src={MenuIcon}
-        className="menu-icon-img"
-        alt="Menu icon button"
-      />
+      <Button className="menu-btn">
+        <img
+          src={MenuIcon}
+          className="menu-icon-img"
+          alt="Menu icon button"
+        />
       </Button>
-      <Menu className='menu-button-menu'>
-        <ul>{menuItems}</ul>
+      <Menu className="menu-button-menu">
+        <ul className="menu-ul">{menuItems}</ul>
       </Menu>
     </Wrapper>
   );
