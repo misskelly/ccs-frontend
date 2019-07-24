7a854e1e9d2f9da2f8ed9bc7efc8f8b8b205c811
Brennan <brennanduffey@gmail.com>
2019-07-23 19:40:34 -0600

    Add closest center location to atlas

--- src/components/Atlas/index.js
+++ src/components/Atlas/index.js
@@ -1,40 +1,14 @@
 import React from 'react';
-import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
+import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
 
 
 
 const Atlas = (props) => {
-  const centerLocations = [
-    {
-      "id": 1,
-      "name": "Denver Walk-In Crisis Services",
-      "phone": 3035041299,
-      "address": "4353 E. Colfax Avenue",
-      "city": "Denver", 
-      "state": "CO", 
-      "zip": 80220,
-      "hours": "24 hours a day, 7 days a week",
-      "lat": 39.7403,
-      "lng": -104.9363
-    },
-    {
-      "id": 2,
-      "name": "Boulder Walk-In Crisis Services",
-      "phone": 3034438500,
-      "address": "3180 Airport Road",
-      "city": "Boulder",
-      "state": "CO",
-      "zip": 80301,
-      "hours": "24 hours a day, 7 days a week",
-      "lat": 40.0330,
-      "lng": -105.2360
-    }
-  ]
 
   return (
     <Map 
       id="mapid" 
-      bounds={[[props.userLocation], [centerLocations[0].lat, centerLocations[0].lng]]} 
+      bounds={[[props.userLocation], [props.closestLocation.lat, props.closestLocation.lng]]}
       center={props.userLocation} zoom={13}
       boundsOptions={{padding: [50, 50]}}>
 
@@ -45,7 +19,7 @@ const Atlas = (props) => {
       <Marker position={props.userLocation}>
         <Popup>Your Location</Popup>
       </Marker>
-      <Marker position={[centerLocations[0].lat, centerLocations[0].lng]} >
+      <Marker position={[props.closestLocation.lat, props.closestLocation.lng]} >
         <Popup>Center Location</Popup>
       </Marker>
     </Map>
