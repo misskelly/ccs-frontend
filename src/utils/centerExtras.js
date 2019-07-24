0caf821d2ef7f00ec1eb573692d01646c45c27e8
Kelly Zick <kllyzck@gmail.com>
2019-07-24 14:42:53 -0600

    Add image urls

--- src/utils/centerExtras.js
+++ src/utils/centerExtras.js
@@ -12,49 +12,49 @@ const centerExtras = [{
   city: 'Boulder',
   name: 'Mental Health Partners Walk-In Center',
   website: 'https://www.mhpcolorado.org/crisis/',
-  logo: Boulder
+  logo: 'https://github.com/CSheesley/ccs-frontend/blob/all-centers-pg/src/assets/images/center-logos/boulder-logo.pnghttps://github.com/CSheesley/ccs-frontend/blob/master/src/assets/images/wheatridge-logo.png?raw=true'
 },
 {
   city: 'Littleton',
   name: 'All Health Network Walk-in Center',
   website: 'https://www.allhealthnetwork.org/services/crisis-services/',
-  logo: Littleton
+  logo: 'https://github.com/CSheesley/ccs-frontend/blob/all-centers-pg/src/assets/images/center-logos/littleton-logo.png?raw=true'
 },
 {
   city: 'Denver',
   name: 'Mental Health Center of Denver',
   website: 'https://mhcd.org/suicide-prevention/',
-  logo: Denver
+  logo: 'https://github.com/CSheesley/ccs-frontend/blob/all-centers-pg/src/assets/images/center-logos/mhcd.jpg?raw=true'
 },
 {
   city: 'Aurora',
   name: 'Aurora Walk-In Crisis Services ',
   website: 'N/A',
-  logo: Aurora
+  logo: 'https://github.com/CSheesley/ccs-frontend/blob/all-centers-pg/src/assets/images/CCS_Horizontal_Color.png?raw=true'
 },
 {
   city: 'Fort Collins',
   name: 'SummitStone Walk-In Crisis Services',
   website: 'https://www.summitstonehealth.org/services/24-7-walk-in-crisis/',
-  logo: FtCollins
+  logo: 'https://github.com/CSheesley/ccs-frontend/blob/all-centers-pg/src/assets/images/center-logos/fort-collins-logo.png?raw=true'
 },
 {
   city: 'Greeley',
   name: 'North Range Crisis Support Services',
   website: 'https://northrange.org/services/crisis-support-services/',
-  logo: Greeley
+  logo: 'https://github.com/CSheesley/ccs-frontend/blob/all-centers-pg/src/assets/images/center-logos/greeley.jpg?raw=true'
 },
 {
   city: 'Pueblo',
   name: 'Crisis Living Room: Emergency Crisis Services',
   website: 'https://www.health.solutions/services/emergency-crisis-services/',
-  logo: Pueblo
+  logo: 'https://github.com/CSheesley/ccs-frontend/blob/all-centers-pg/src/assets/images/center-logos/pueblo-logo.png?raw=true'
 },
 {
   city: 'Colorado Springs',
   name: 'Crisis Living Room: Emergency Crisis Services',
   website: 'https://www.aspenpointe.org/health-care-services/crisis-services-(text-call-walk-in)/crisis-stabilization-unit',
-  logo: CoSprings
+  logo: 'https://github.com/CSheesley/ccs-frontend/blob/all-centers-pg/src/assets/images/center-logos/colorado-springs-logo.png?raw=true'
 },
 // {
 //   city: 'Montrose',
@@ -66,7 +66,7 @@ const centerExtras = [{
   city: 'Wheat Ridge',
   name: 'Center for Mental Health Crisis Walk-In Center',
   website: 'https://www.centermh.org/crisis-walk-in-center/',
-  logo: Wheatridge
+  logo: 'https://github.com/CSheesley/ccs-frontend/blob/all-centers-pg/src/assets/images/center-logos/wheatridge-logo.png?raw=true'
 }
 
 ];
